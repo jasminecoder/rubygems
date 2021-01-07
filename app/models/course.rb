5647fc490e3c3c25778d5871e66927c1c8a42776
Jasmine Coder <jasminethecoder@gmail.com>
2021-01-07 11:52:30 -0800

    add a lessons table, and friendly id for lessons

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,6 +3,7 @@ class Course < ApplicationRecord
     validates :description, presence: true, length: {:minimum => 5}
     has_rich_text :description
     belongs_to :user
+    has_many :lessons, dependent: :destroy
     extend FriendlyId
     friendly_id :title, use: :slugged
     def to_s 
