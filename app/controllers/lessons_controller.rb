0cb4134dc78ae409bcfe6dd069c874ed5317aec1
Jasmine Coder <jasminethecoder@gmail.com>
2021-01-08 15:48:08 -0800

    authorization for lessons create

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -11,8 +11,6 @@ class LessonsController < ApplicationController
   # GET /lessons/1.json
   def show
     authorize @lesson
-    # @course = @lesson.course
-    # @lessons = @course.lessons
   end
 
   # GET /lessons/new
@@ -32,6 +30,7 @@ class LessonsController < ApplicationController
     @lesson = Lesson.new(lesson_params)
     @course = Course.friendly.find(params[:course_id])
     @lesson.course_id = @course.id
+    authorize @lesson
     respond_to do |format|
       if @lesson.save
         format.html { redirect_to course_lesson_path(@course, @lesson), notice: 'Lesson was successfully created.' }
@@ -64,7 +63,7 @@ class LessonsController < ApplicationController
     authorize @lesson
     @lesson.destroy
     respond_to do |format|
-      format.html { redirect_to lessons_url, notice: 'Lesson was successfully destroyed.' }
+      format.html { redirect_to course_path(@course), notice: 'Lesson was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -78,6 +77,6 @@ class LessonsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def lesson_params
-      params.require(:lesson).permit(:title, :content, :course_id)
+      params.require(:lesson).permit(:title, :content)
     end
 end
