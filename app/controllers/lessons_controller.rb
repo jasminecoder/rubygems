42064de312c4555c6b5169f71922427d8ecfe4b4
Jasmine Coder <jasminethecoder@gmail.com>
2021-01-08 09:53:42 -0800

    nest lessons in courses

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -11,13 +11,14 @@ class LessonsController < ApplicationController
   # GET /lessons/1.json
   def show
     authorize @lesson
-    @course = @lesson.course
-    @lessons = @course.lessons
+    # @course = @lesson.course
+    # @lessons = @course.lessons
   end
 
   # GET /lessons/new
   def new
     @lesson = Lesson.new
+    @course = Course.friendly.find(params[:course_id])
   end
 
   # GET /lessons/1/edit
@@ -29,10 +30,11 @@ class LessonsController < ApplicationController
   # POST /lessons.json
   def create
     @lesson = Lesson.new(lesson_params)
-
+    @course = Course.friendly.find(params[:course_id])
+    @lesson.course_id = @course.id
     respond_to do |format|
       if @lesson.save
-        format.html { redirect_to @lesson, notice: 'Lesson was successfully created.' }
+        format.html { redirect_to course_lesson_path(@course, @lesson), notice: 'Lesson was successfully created.' }
         format.json { render :show, status: :created, location: @lesson }
       else
         format.html { render :new }
@@ -47,7 +49,7 @@ class LessonsController < ApplicationController
     authorize @lesson
     respond_to do |format|
       if @lesson.update(lesson_params)
-        format.html { redirect_to @lesson, notice: 'Lesson was successfully updated.' }
+        format.html { redirect_to course_lesson_path(@course, @lesson), notice: 'Lesson was successfully updated.' }
         format.json { render :show, status: :ok, location: @lesson }
       else
         format.html { render :edit }
@@ -70,6 +72,7 @@ class LessonsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_lesson
+      @course = Course.friendly.find(params[:course_id])
       @lesson = Lesson.friendly.find(params[:id])
     end
 
