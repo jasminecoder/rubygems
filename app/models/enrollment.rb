abc64e572a2705e6dca8b68f31b4c1120a778db5
Jasmine Coder <jasminethecoder@gmail.com>
2021-01-14 10:16:55 -0800

    add friendly_id to enrollments

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -2,6 +2,7 @@ class Enrollment < ApplicationRecord
   belongs_to :course
   belongs_to :user
   
+
   validates :user, :course, presence: true
 
   validates_uniqueness_of :user_id, scope: :course_id
@@ -10,7 +11,9 @@ class Enrollment < ApplicationRecord
   validate :cant_subscribe_to_own_course
 
   scope :pending_review, -> { where(rating: [0, nil, "], review: [0, nil, "]) }
- 
+  
+  extend FriendlyId
+  friendly_id :to_s, use: :slugged
 
 
   def to_s 
